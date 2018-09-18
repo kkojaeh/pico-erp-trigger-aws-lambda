7dcc00bcf0f31f7f974ea14b76fe372e9fe1ecc7
Ethan <kkojaeh@gmail.com>
2018-09-18 16:47:54 +0900

    작업일 자동 생성 추가

---
+++ generate-work-days.js
@@ -0,0 +1,15 @@
+const api = require('./api')
+const moment = require('moment')
+module.exports.execute = async () => {
+  // 이번달 1일
+  const begin = moment().date(1)
+  // 3개월 뒤 마지막 날
+  const end = moment().add(3, 'month').date(0)
+  const response = await api.execute('/work-day/generate-work-days', 'POST', {
+    begin: begin.format('YYYY-MM-DD'),
+    end: end.format('YYYY-MM-DD')
+  })
+  if (!response.ok) {
+    throw new Error(response.statusText)
+  }
+}
--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const clearAttachments = require('./clear-attachments')
 const expireItemLots = require('./expire-item-lots')
 const expireQuotations = require('./expire-quotations')
+const generateWorkDays = require('./generate-work-days')
 
 exports.clearAttachments = clearAttachments.execute
 exports.expireItemLots = expireItemLots.execute
 exports.expireQuotations = expireQuotations.execute
+exports.generateWorkDays = generateWorkDays.execute
